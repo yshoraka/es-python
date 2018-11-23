c7664e921350fd3bd2b2f4f114072542ccbbd6c1
Yashar Shoraka <yshoraka@gmail.com>
2018-11-23 13:22:48 +1100

    Adding initial codes from a local computer

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,request
+from elasticsearch import Elasticsearch
+
+
+app = Flask(__name__)
+es = Elasticsearch()
+HEADERS = {'content-type': 'application/json'}
+
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    q = request.form.get("q")
+
+    if q is not None:
+        es.transport.connection_pool.connection.headers.update(HEADERS)
+        resp = es.search(index="products",doc_type="default",body={"query": {"match": {"name.keyword": q}}})
+        return render_template("index.html",q=q,response=resp)
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8000)
